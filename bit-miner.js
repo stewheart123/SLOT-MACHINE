1edda01c4508da22758996e0eef0fd04bc3e1e17
Stewart <44583600+stewheart123@users.noreply.github.com>
2023-05-13 20:27:36 +0100

    working slot integration
    need to clean up the reels by adding a mask
    plus fix the issues of the game monitor moving after first load

--- bit-miner.js
+++ bit-miner.js
@@ -6,6 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
  let hasWatts = false;
  let watts = 0;
  let funds = 1000;
+ const reels = [];
+
+ PIXI.Assets.load([
+    'assets/images/red.png',
+    'assets/images/orange.png',
+    'assets/images/blue.png',
+    'assets/images/green.png',
+]).then(onAssetsLoaded);
 
  //game states
  let spinning = false;
@@ -21,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
  });
 
  document.body.appendChild(app.view);
-
+ function onAssetsLoaded() {
  const bgSprite = PIXI.Sprite.from(
    "assets/images/background-server-room.jpg"
  );
@@ -95,7 +103,7 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
         gameDisplayMonitor.position.set(
             rackContainer.width / 2 - gameDisplayMonitor.width / 2,
-            150
+            50
           );
     }
   
@@ -255,13 +263,172 @@ function buyWatts(){
 function spin(){
     console.log('spin called!');    
     if(watts >= 100) {
-        spinning = true;
+      //  spinning = true;
         watts -=100;
+        startPlay();
         setGameInfoText();      
-        setRackState();
+       
+        
     }
     if(spinning) {
         mineButtonSprite.interactive = false;
         mineButtonSprite.filters = [alphaFilterInactive];
     }
+}
+function setMineButtonState() {
+    if(watts < 1) {
+        mineButtonSprite.interactive = false;
+        mineButtonSprite.filters = [alphaFilterInactive];
+    } else {
+        mineButtonSprite.interactive = true;
+        mineButtonSprite.filters = [alphaFilterFull];
+    }
+}
+//slot demo integration
+
+const REEL_WIDTH = gameDisplayMonitor.width / 5;
+const SYMBOL_SIZE = gameDisplayMonitor.width/ 5.1;
+
+// onAssetsLoaded handler builds the example.
+
+    // Create different slot symbols.
+    const slotTextures = [
+        PIXI.Texture.from('assets/images/red.png'),
+        PIXI.Texture.from('assets/images/orange.png'),
+        PIXI.Texture.from('assets/images/blue.png'),
+        PIXI.Texture.from('assets/images/green.png'),
+    ];
+
+    // Build the reels
+    
+    const reelContainer = new PIXI.Container();
+    for (let i = 0; i < 5; i++) {
+        const rc = new PIXI.Container();
+        rc.x = i * REEL_WIDTH;
+        reelContainer.addChild(rc);
+
+        const reel = {
+            container: rc,
+            symbols: [],
+            position: 0,
+            previousPosition: 0,
+            blur: new PIXI.filters.BlurFilter(),
+        };
+        reel.blur.blurX = 0;
+        reel.blur.blurY = 0;
+        rc.filters = [reel.blur];
+
+        // Build the symbols
+        for (let j = 0; j < 4; j++) {
+            const symbol = new PIXI.Sprite(slotTextures[Math.floor(Math.random() * slotTextures.length)]);
+            // Scale the symbol to fit symbol area.
+            symbol.y = j * SYMBOL_SIZE;
+            symbol.scale.x = symbol.scale.y = Math.min(SYMBOL_SIZE / symbol.width, SYMBOL_SIZE / symbol.height);
+            symbol.x = Math.round((SYMBOL_SIZE - symbol.width) / 2);
+            reel.symbols.push(symbol);
+            rc.addChild(symbol);
+        }
+        reels.push(reel);
+    }
+    gameDisplayMonitor.addChild(reelContainer);
+
+    // Reels done handler.
+    function reelsComplete() {
+        spinning = false;
+        setMineButtonState();
+        setRackState();
+    }
+
+    // Listen for animate update.
+    app.ticker.add((delta) => {
+    // Update the slots.
+        for (let i = 0; i < reels.length; i++) {
+            const r = reels[i];
+            // Update blur filter y amount based on speed.
+            // This would be better if calculated with time in mind also. Now blur depends on frame rate.
+            r.blur.blurY = (r.position - r.previousPosition) * 8;
+            r.previousPosition = r.position;
+
+            // Update symbol positions on reel.
+            for (let j = 0; j < r.symbols.length; j++) {
+                const s = r.symbols[j];
+                const prevy = s.y;
+                s.y = ((r.position + j) % r.symbols.length) * SYMBOL_SIZE - SYMBOL_SIZE;
+                if (s.y < 0 && prevy > SYMBOL_SIZE) {
+                    // Detect going over and swap a texture.
+                    // This should in proper product be determined from some logical reel.
+                    s.texture = slotTextures[Math.floor(Math.random() * slotTextures.length)];
+                    s.scale.x = s.scale.y = Math.min(SYMBOL_SIZE / s.texture.width, SYMBOL_SIZE / s.texture.height);
+                    s.x = Math.round((SYMBOL_SIZE - s.width) / 2);
+                }
+            }
+        }
+    });
+
+
+
+
+// Very simple tweening utility function. This should be replaced with a proper tweening library in a real product.
+const tweening = [];
+function tweenTo(object, property, target, time, easing, onchange, oncomplete) {
+    const tween = {
+        object,
+        property,
+        propertyBeginValue: object[property],
+        target,
+        easing,
+        time,
+        change: onchange,
+        complete: oncomplete,
+        start: Date.now(),
+    };
+
+    tweening.push(tween);
+    return tween;
+}
+// Listen for animate update.
+app.ticker.add((delta) => {
+    const now = Date.now();
+    const remove = [];
+    for (let i = 0; i < tweening.length; i++) {
+        const t = tweening[i];
+        const phase = Math.min(1, (now - t.start) / t.time);
+
+        t.object[t.property] = lerp(t.propertyBeginValue, t.target, t.easing(phase));
+        if (t.change) t.change(t);
+        if (phase === 1) {
+            t.object[t.property] = t.target;
+            if (t.complete) t.complete(t);
+            remove.push(t);
+        }
+    }
+    for (let i = 0; i < remove.length; i++) {
+        tweening.splice(tweening.indexOf(remove[i]), 1);
+    }
+});
+
+// Basic lerp funtion.
+function lerp(a1, a2, t) {
+    return a1 * (1 - t) + a2 * t;
+}
+
+// Backout function from tweenjs.
+// https://github.com/CreateJS/TweenJS/blob/master/src/tweenjs/Ease.js
+function backout(amount) {
+    return (t) => (--t * t * ((amount + 1) * t + amount) + 1);
+}
+
+// Function to start playing.
+function startPlay() {
+    if (spinning) return;
+    spinning = true;
+
+    for (let i = 0; i < reels.length; i++) {
+        const r = reels[i];
+        const extra = Math.floor(Math.random() * 3);
+        const target = r.position + 10 + i * 5 + extra;
+        const time = 2500 + i * 600 + extra * 600;
+        tweenTo(r, 'position', target, time, backout(0.5), null, i === reels.length - 1 ? reelsComplete : null);
+    }
+}
 }
